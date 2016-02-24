00467ec235d06a0603000ce2f04107c0ce3436da
Leonardo <leonardo.filippini91@gmail.com>
2016-02-24 17:22:00 +0100

    branch

--- monopoly_r1/src/monopoly/Board.java
+++ monopoly_r1/src/monopoly/Board.java
@@ -1,5 +1,16 @@
 package monopoly;
 
-public class Board {
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class Board extends JPanel implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
